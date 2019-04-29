c0bc4a9835cadad01b898f932f9e53a9a1f11e35
ibrahim karim othman <ibrahimothmanre@gmail.com>
2019-04-29 13:52:16 +0200

    More in Cart feature
    - Implement add_to_cart_btn functionality

--- app/src/main/java/com/example/woocommerce/ui/ProductDetailActivity.java
+++ app/src/main/java/com/example/woocommerce/ui/ProductDetailActivity.java
@@ -1,5 +1,8 @@
 package com.example.woocommerce.ui;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +11,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.woocommerce.R;
 import com.example.woocommerce.adapter.DetailsAdapter;
 import com.example.woocommerce.model.Product;
 import com.example.woocommerce.utils.PrefManager;
+import com.example.woocommerce.viewmodel.ProducDetailViewModel;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -22,6 +27,7 @@ public class ProductDetailActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tabLayout;
     Button mAddToCartBtn;
+    ProducDetailViewModel mViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +38,9 @@ public class ProductDetailActivity extends AppCompatActivity {
         tabLayout=findViewById(R.id.product_detail_tabLayout);
         mAddToCartBtn=findViewById(R.id.addToCart);
 
+
+        mViewModel = ViewModelProviders.of(this).get(ProducDetailViewModel.class);
+
         product =getIntent().getParcelableExtra(PRODUCT_KEY);
         Log.d("PRODUCCTTT", "product : "+product.getName());
         populateProductDetail(product);
@@ -39,7 +48,18 @@ public class ProductDetailActivity extends AppCompatActivity {
         mAddToCartBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mAddToCartBtn.setClickable(false);
+                mViewModel.addProductToCart(product.getId(),1);
+                mViewModel.getIsItemSavedIntoCart().observe(ProductDetailActivity.this, new Observer<Boolean>() {
+                    @Override
+                    public void onChanged(@Nullable Boolean aBoolean) {
+                        if(aBoolean){
+                            Toast.makeText(ProductDetailActivity.this, "product is successfully added to your cart", Toast.LENGTH_SHORT).show();
+                        }else Toast.makeText(ProductDetailActivity.this, "somthing went wrong, please try again", Toast.LENGTH_SHORT).show();
 
+                        mAddToCartBtn.setClickable(true);
+                    }
+                });
             }
         });
     }
