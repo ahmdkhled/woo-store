8a71a0faaf8cc271eead4e626cef6a3fcc67bdc4
ibrahim karim othman <ibrahimothmanre@gmail.com>
2019-04-29 17:17:58 +0200

    More in Cart feature
    - Finish cart_activity_layout design

--- app/src/main/java/com/example/woocommerce/ui/CartActivity.java
+++ app/src/main/java/com/example/woocommerce/ui/CartActivity.java
@@ -1,8 +1,10 @@
 package com.example.woocommerce.ui;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.woocommerce.model.CartItem;
 import com.example.woocommerce.utils.PrefManager;
@@ -18,6 +20,13 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-        
+        // hide status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
     }
 }
