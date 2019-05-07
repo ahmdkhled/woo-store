6fa7b7261e5184a0e59b7e86f386ea55d0199132
ahmdkhled <ahmdkhled@gmail.com>
2019-05-07 20:38:24 +0200

    handle adding address during checkout

---
+++ app/src/main/java/com/example/woocommerce/model/Billing.java
@@ -0,0 +1,76 @@
+package com.example.woocommerce.model;
+
+public class Billing {
+    private String first_name;
+    private String last_name;
+    private String company;
+    private String address_1;
+    private String address_2;
+    private String city;
+    private String state;
+    private String postcode;
+    private String country;
+    private String email;
+    private String phone;
+
+
+    public Billing(String first_name, String last_name, String address_1,
+                   String address_2, String city, String state,
+                   String postcode, String country,
+                   String email, String phone) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address_1 = address_1;
+        this.address_2 = address_2;
+        this.city = city;
+        this.state = state;
+        this.postcode = postcode;
+        this.country = country;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress_1() {
+        return address_1;
+    }
+
+    public String getAddress_2() {
+        return address_2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
--- app/src/main/java/com/example/woocommerce/ui/CheckoutActivity.java
+++ app/src/main/java/com/example/woocommerce/ui/CheckoutActivity.java
@@ -3,20 +3,35 @@ package com.example.woocommerce.ui;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import com.example.woocommerce.R;
 import com.example.woocommerce.adapter.CheckoutAdapter;
 
-public class CheckoutActivity extends AppCompatActivity {
+public class CheckoutActivity extends AppCompatActivity implements AddressFrag.OnAddressFieldsValidated{
 
     ViewPager recyclerView;
+     Button proceedToPayment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
         recyclerView=findViewById(R.id.checkout_pager);
+        proceedToPayment=findViewById(R.id.proceedToPayment);
 
         CheckoutAdapter adapter=new CheckoutAdapter(getSupportFragmentManager());
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onAddressFieldsValidated() {
+        Log.d("ADDRREESSS", "onAddressFieldsValidated: ");
+    }
+
+    @Override
+    public void onAddressFieldsMissing() {
+        Log.d("ADDRREESSS", "onAddressFieldsMissing: ");
     }
 }
